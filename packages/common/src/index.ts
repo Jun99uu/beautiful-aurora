200c5aa6372b6a814c7180698eb03d3e1c9b73b8
Jun99uu_Lee <igun0423@naver.com>
2023-08-09 22:24:28 +0900

    ➕ preset-react babel

---
+++ packages/common/src/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./components/Buttons/Button";
+export { default as CheckBox } from "./components/Buttons/CheckBox";
+export { default as Recommend } from "./components/Colors/Recommend";
+export { default as SliderControl } from "./components/Colors/SliderControl";
+export { default as TextInput } from "./components/Field";
