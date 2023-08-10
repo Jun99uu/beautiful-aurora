aa8274dd612d184b929cf0064ec94e1e021c786c
Jun99uu_Lee <igun0423@naver.com>
2023-08-11 00:37:31 +0900

    ✨ complete modal

--- packages/client/src/components/Popup/Modal.tsx
+++ packages/client/src/components/Popup/Modal.tsx
@@ -3,9 +3,10 @@ import Portal from "./Portal";
 import "./modal.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
-import { ChangeEvent, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useRef, useState } from "react";
 import { useOutsideClose } from "../hooks/useOutsideClose";
 import { useGenerator } from "@/hook/useGenerator";
+import complete from "@assets/complete.png";
 
 interface Props {
   handleClose: () => void;
@@ -19,6 +20,7 @@ const Modal = ({ handleClose }: Props) => {
     "React & emotion (css)",
   ];
   const [selectIdx, setSelectIdx] = useState(-1);
+  const [stage, setStage] = useState("select");
   const { copy } = useGenerator();
 
   const ref = useRef<HTMLDivElement>(null);
@@ -29,30 +31,49 @@ const Modal = ({ handleClose }: Props) => {
   };
 
   const copyToClipboard = () => {
-    if (selectIdx !== -1) {
+    if (selectIdx !== -1 && stage === "select") {
       copy(selectIdx);
+      setStage("complete");
+    }
+    if (stage === "complete") {
       handleClose();
     }
   };
+
+  const StageComponent = useCallback(() => {
+    switch (stage) {
+      case "select":
+        return list.map((item, idx) => (
+          <CheckBox
+            content={item}
+            checked={idx === selectIdx}
+            onChange={(e) => handleSelect(e, idx)}
+          />
+        ));
+      case "complete":
+        return (
+          <div className="complete-box">
+            <img src={complete} alt="complete" />
+            <span>Copied to Clipboard!</span>
+          </div>
+        );
+    }
+  }, [stage, selectIdx, list]);
 
   return (
     <Portal wrapperId="modal-wrapper">
       <div className="modal-background">
         <div className="modal-wrapper" ref={ref}>
           <FontAwesomeIcon icon={faXmark} className="x" onClick={handleClose} />
-          {list.map((item, idx) => (
-            <CheckBox
-              content={item}
-              checked={idx === selectIdx}
-              onChange={(e) => handleSelect(e, idx)}
-            />
-          ))}
+          <StageComponent />
           <Button
             size="large"
             style={{ marginTop: "30px" }}
             onClick={copyToClipboard}
           >
-            <span>click to copy</span>
+            <span>
+              {stage === "select" ? "click to copy" : "close this modal"}
+            </span>
           </Button>
         </div>
       </div>
