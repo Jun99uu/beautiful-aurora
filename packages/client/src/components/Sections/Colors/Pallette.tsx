f79998498bd1c983d4b560165823771affebd654
Jun99uu_Lee <igun0423@naver.com>
2023-08-10 23:07:05 +0900

    ✨ ➕ Add Color-picker

--- packages/client/src/components/Sections/Colors/Pallette.tsx
+++ packages/client/src/components/Sections/Colors/Pallette.tsx
@@ -2,9 +2,23 @@ import { colorState } from "@/store/colorAtom";
 import { SliderControl } from "@aurora/common";
 import { useAtom } from "jotai";
 import "./colors.scss";
+import { ChromePicker } from "react-color";
+import { useRef, useState } from "react";
+import { useOutsideClose } from "@/components/hooks/useOutsideClose";
 
 const Pallette = () => {
   const [colors, setColors] = useAtom(colorState);
+  const [openIdx, setOpenIdx] = useState(-1);
+  const ref = useRef(null);
+
+  const openPallette = (idx: number) => {
+    setOpenIdx(idx);
+  };
+  const closePallette = () => {
+    setOpenIdx(-1);
+  };
+
+  useOutsideClose(ref, closePallette);
 
   const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = parseInt(event.target.value, 10);
@@ -12,6 +26,28 @@ const Pallette = () => {
       return {
         ...prev,
         transperant: newValue,
+      };
+    });
+  };
+
+  const handleColorByIndex = (hex: string, idx: number) => {
+    if (idx >= 0 && idx < colors.color.length) {
+      let newColors = colors.color;
+      newColors[idx] = hex;
+      setColors((prev) => {
+        return {
+          ...prev,
+          color: newColors,
+        };
+      });
+    }
+  };
+
+  const handleColorBg = (hex: string) => {
+    setColors((prev) => {
+      return {
+        ...prev,
+        bg: hex,
       };
     });
   };
@@ -24,11 +60,37 @@ const Pallette = () => {
           <span className="caption">Background</span>
         </div>
         <div className="color-wrapper">
-          {colors.color.map((co) => (
-            <div className="color-ball" style={{ backgroundColor: co }} />
+          {colors.color.map((co, idx) => (
+            <div
+              onClick={() => openPallette(idx)}
+              className="color-ball"
+              style={{ backgroundColor: co }}
+            >
+              {openIdx === idx && (
+                <span ref={ref} className="color-picker">
+                  <ChromePicker
+                    color={co}
+                    onChange={(color) => handleColorByIndex(color.hex, idx)}
+                  />
+                </span>
+              )}
+            </div>
           ))}
           <span className="vertical" />
-          <div className="color-ball" style={{ backgroundColor: colors.bg }} />
+          <div
+            onClick={() => openPallette(4)}
+            className="color-ball"
+            style={{ backgroundColor: colors.bg }}
+          >
+            {openIdx === 4 && (
+              <span ref={ref} className="color-picker">
+                <ChromePicker
+                  color={colors.bg}
+                  onChange={(color) => handleColorBg(color.hex)}
+                />
+              </span>
+            )}
+          </div>
         </div>
       </div>
       <div className="item-container">
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@aurora/common/dist/assets/output-1d77dc77.css";
+import "@aurora/common/dist/assets/output-5d7c4f70.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "@styles/global.scss";
 
