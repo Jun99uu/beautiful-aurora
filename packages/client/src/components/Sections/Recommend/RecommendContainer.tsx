1cdc58379533a508645360def5d81ed391adaa66
Jun99uu_Lee <igun0423@naver.com>
2023-08-12 10:13:22 +0900

    👔 Fix view color list

--- packages/client/src/components/Sections/Recommend/RecommendContainer.tsx
+++ packages/client/src/components/Sections/Recommend/RecommendContainer.tsx
@@ -5,24 +5,24 @@ import { useAtom } from "jotai";
 import { colorState } from "@/store/colorAtom";
 
 const RecommendContainer = () => {
-  const newArr = auroraColors.slice(0, 4);
+  const [viewColorList, setViewColorList] = useState(auroraColors.slice(0, 4));
   const [_, setColors] = useAtom(colorState);
   const [isSelected, setIsSelected] = useState(-1);
 
   const handleSelectChange = (idx: number) => {
     setIsSelected(idx);
-    setColors(newArr[idx]);
+    setColors(viewColorList[idx]);
   };
 
   return (
     <div className="recommend-box">
-      {newArr.map((color, idx) => (
+      {viewColorList.map((color, idx) => (
         <Recommend
           isSelected={idx === isSelected}
           firstColor={color.color[0]}
-          secondColor={color.color[0]}
-          thirdColor={color.color[0]}
-          fourthColor={color.color[0]}
+          secondColor={color.color[1]}
+          thirdColor={color.color[2]}
+          fourthColor={color.color[3]}
           backgroundColor={color.bg}
           onClick={() => handleSelectChange(idx)}
         />
